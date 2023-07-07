97c3461c3b665a50d2cf70d21a3f7065cecbd34c
Firo91 <112562425+Firo91@users.noreply.github.com>
2023-07-07 16:31:15 +0200

    Files upload
    Files upload

--- FileShare/FileShare/settings.py
+++ FileShare/FileShare/settings.py
@@ -134,7 +134,3 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import os
-if 'WEBSITE_HOSTNAME' in os.environ: # Running on Azure
-    from .azure import *
